f2f0460f82ff3b488f07c9594040b0fb2f4012b1
Ripunjoy <ripunjoysarma2000@gmail.com>
2021-05-26 19:38:17 +0530

    show msg when a new user is joined and and when a user left

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,13 @@
 const socket=io()
 
-socket.on('countUpdated',(count)=>
-{
-    console.log('The count has been updated ',count)
-})
 
-document.querySelector('#incrementcount').addEventListener('click',(count)=>
-    {
-        console.log('clicked');
-        socket.emit('increment');
-    })
+document.querySelector("#sub").addEventListener('click',(e)=>
+{
+    e.preventDefault();
+    const msg=document.querySelector('input').value;
+    socket.emit('message',msg);
+})
+socket.on('show_msg',(value)=>
+{
+    console.log(value);
+})
